8f8abad49af8bb67d587917343db180629cf8c53
GitHub Action <action@github.com>
2023-01-28 18:18:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/astrapi69/fileformattransformer/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/astrapi69/fileformattransformer/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.astrapi69.fileformattransformer.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.astrapi69.fileformattransformer.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/astrapi69/fileformattransformer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/astrapi69/fileformattransformer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.astrapi69.fileformattransformer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.astrapi69.fileformattransformer.MyBundle
 
 class MyProjectService(project: Project) {
 
