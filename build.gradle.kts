afdcabef876f1c371a7d69bafb47ef9d106ac218
astrapi69 <asterios.raptis@gmx.net>
2023-04-04 17:33:25 +0200

    new action that converts json to properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ kotlin {
 
 dependencies {
     implementation("io.github.astrapi69:json-extensions:2.4")
+    implementation("io.github.astrapi69:gson-extensions:2")
     implementation("io.github.astrapi69:xml-extensions:8.2")
     implementation("io.github.astrapi69:yaml-to-prop-to-yaml:2")
     implementation("io.github.astrapi69:silly-io:2.2")
---
+++ src/main/kotlin/com/github/astrapi69/fileformattransformer/action/JsonFileToPropertiesContextMenuAction.kt
@@ -0,0 +1,48 @@
+package com.github.astrapi69.fileformattransformer.action
+
+import com.github.astrapi69.fileformattransformer.setting.ApplicationSettingsState
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
+import io.github.astrapi69.collection.properties.PropertiesExtensions
+import io.github.astrapi69.gson.JsonToPropertiesExtensions
+import io.github.astrapi69.io.StringOutputStream
+import io.github.astrapi69.json.JsonToYamlExtensions
+
+class JsonFileToPropertiesContextMenuAction: AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val it: VirtualFile? = event.getData(CommonDataKeys.VIRTUAL_FILE)
+        if(it != null) {
+            val jsonString = VfsUtilCore.loadText(it)
+            val properties = JsonToPropertiesExtensions.toProperties(jsonString)
+            val outputStream = StringOutputStream()
+            PropertiesExtensions.export(properties, outputStream)
+            val propertiesAsString = outputStream.toString()
+
+            WriteAction.run<Throwable> {
+                if (ApplicationSettingsState.instance.newFile) {
+                    val createChildSequent: VirtualFile = VfsUtil.createChildSequent(
+                        event.project,
+                        it.parent, it.nameWithoutExtension, "properties"
+                    )
+                    VfsUtil.saveText(createChildSequent, propertiesAsString)
+                } else {
+                    VfsUtil.saveText(it, propertiesAsString)
+                    it.rename(it, it.nameWithoutExtension + ".properties")
+                }
+            }
+        }
+    }
+
+    override fun update(event: AnActionEvent) {
+        val it = event.getData(CommonDataKeys.VIRTUAL_FILE)
+        if(it != null) {
+            event.presentation.isEnabledAndVisible = it.extension.equals("json")
+        }
+    }
+}
